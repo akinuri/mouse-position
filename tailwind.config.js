4f01cd3536e25acf8b36dcc799909ea40f9335cd
Noreh AD <akinoreh@gmail.com>
2024-02-01 20:53:16 +0300

    mark event target

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    safelist: [
+        "outline",
+        "outline-2",
+        "outline-dashed",
+        "outline-gray-800/80",
+        "-outline-offset-1",
+    ]
 }
 
